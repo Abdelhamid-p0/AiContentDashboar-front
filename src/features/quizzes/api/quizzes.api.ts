253328c48971c36f4fc273a57031f266c0c5ad8a
Abdelhamid-p0 <abdelhamid.hachimi.alaoui@gmail.com>
2026-06-01 13:25:42 +0200

    Deploy front to GitHub Pages

--- src/features/quizzes/api/quizzes.api.ts
+++
@@ -1,6 +0,0 @@
-import { fetchJson } from '@/shared/lib/http'
-import type { QuizGroupApiResponse } from '../model/quiz.types'
-
-export async function getCourseQuizzes(courseId: string): Promise<QuizGroupApiResponse> {
-  return fetchJson<QuizGroupApiResponse>(`/v1/courses/${courseId}/quizzes`)
-}
